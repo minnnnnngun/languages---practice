d78d6096fc93d3b22e5fbf6cf33e74b984f9ba6d
minnnnnngun <20261305@dsm.hs.kr>
2026-03-10 21:58:01 +0900

    calculator.c

---
+++ calculator.c
@@ -0,0 +1,16 @@
+int main() {
+    int num1, num2;
+    int result;
+
+    printf("첫 번째 숫자를 입력하세요: ");
+    scanf("%d", &num1);
+
+    printf("두 번째 숫자를 입력하세요: ");
+    scanf("%d", &num2);
+
+    result = num1 + num2;
+
+    printf("결과: %d\n", result);
+
+    return 0;
+}

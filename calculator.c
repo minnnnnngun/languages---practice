ba0ed25ec57b3852ef8efe29b39eb4eda1f8b7d7
minnnnnngun <20261305@dsm.hs.kr>
2026-03-10 22:07:11 +0900

    calculator.c

--- calculator.c
+++ calculator.c
@@ -1,16 +1,18 @@
+#include <stdio.h>
+
 int main() {
     int num1, num2;
-    int result;
 
-    printf("첫 번째 숫자를 입력하세요: ");
+    printf("첫 번째 숫자: ");
     scanf("%d", &num1);
 
-    printf("두 번째 숫자를 입력하세요: ");
+    printf("두 번째 숫자: ");
     scanf("%d", &num2);
 
-    result = num1 + num2;
-
-    printf("결과: %d\n", result);
+    printf("덧셈: %d\n", num1 + num2);
+    printf("뺄셈: %d\n", num1 - num2);
+    printf("곱셈: %d\n", num1 * num2);
+    printf("나눗셈: %d\n", num1 / num2);
 
     return 0;
 }
